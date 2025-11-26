ec1deccd030bb00b27d6064a9bbc6108cb770e7a
Jeansld <147097693+Jeansld@users.noreply.github.com>
2025-11-25 21:21:12 -0500

    Corregida la importación de 'vite-plugin-sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,27 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+
+import sitemap from "vite-plugin-sitemap";
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+
+   
+    mode === "production" &&
+    sitemap({
+      hostname: "https://fibrasmontoya.com/",
+      exclude: ["/404"],
+    }),
+
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
